713db09adb573cce6713393b9d9b2e8417327283
impie66 <inomoncrayons@hotmail.com>
2018-06-13 19:00:49 +1000

    Added my batch file
    Why the fuck not

---
+++ usefullbatchfile.bat
@@ -0,0 +1,45 @@
+@echo off
+@color 0A
+
+:menu
+cls
+echo 1. Purple
+echo 2. Byran
+echo 3. Ecgberth
+echo 4. ifLocal
+echo 5. reset
+set /p input="Action?"
+if %input%==1 (goto:purple)
+if %input%==2 (goto:Byran)
+if %input%==3 (goto:Eggbirth)
+if %input%==4 (goto:ifLocal)
+if %input%==5 (goto:reset)
+if %input% LEQ 0 (goto:menu)
+echo %input%
+
+
+
+:purple
+xcopy "E:\Starcraft\Brood War\bwapi-data\batch copies\Purplewave\bwapi.ini" "E:\Starcraft\Brood War\bwapi-data\bwapi.ini"
+echo purple
+goto menu
+
+:Byran
+xcopy "E:\Starcraft\Brood War\bwapi-data\batch copies\bryan\bwapi.ini" "E:\Starcraft\Brood War\bwapi-data\bwapi.ini"
+echo bryan
+goto menu
+
+:Eggbirth
+xcopy "E:\Starcraft\Brood War\bwapi-data\batch copies\Jabbo\bwapi.ini" "E:\Starcraft\Brood War\bwapi-data\bwapi.ini"
+echo egg
+goto menu
+
+:ifLocal
+xcopy "E:\Starcraft\Brood War\bwapi-data\batch copies\ifLocal\bwapi.ini" "E:\Starcraft\Brood War\bwapi-data\bwapi.ini"
+echo ifLocal
+goto menu
+
+:reset
+xcopy "E:\Starcraft\Brood War\bwapi-data\batch copies\reset\bwapi.ini" "E:\Starcraft\Brood War\bwapi-data\bwapi.ini"
+echo reset
+goto menu
