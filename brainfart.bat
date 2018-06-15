d6996a852024a697ccb67a0414e95d38857b48fc
impie66 <inomoncrayons@hotmail.com>
2018-06-15 23:03:53 +1000

    WHY CANT I SPELL MY OWN NAME RIGHT

--- brainfart.bat
+++ brainfart.bat
@@ -4,13 +4,13 @@
 :menu
 cls
 echo 1. Purple
-echo 2. Byran
+echo 2. Bryan
 echo 3. Ecgberth
 echo 4. ifLocal
 echo 5. reset
 set /p input="Action?"
 if %input%==1 (goto:purple)
-if %input%==2 (goto:Byran)
+if %input%==2 (goto:Bryan)
 if %input%==3 (goto:Eggbirth)
 if %input%==4 (goto:ifLocal)
 if %input%==5 (goto:reset)
@@ -24,7 +24,7 @@ xcopy "E:\Starcraft\Brood War\bwapi-data\batch copies\Purplewave\bwapi.ini" "E:\
 echo purple
 goto menu
 
-:Byran
+:Bryan
 xcopy "E:\Starcraft\Brood War\bwapi-data\batch copies\bryan\bwapi.ini" "E:\Starcraft\Brood War\bwapi-data\bwapi.ini"
 echo bryan
 goto menu
